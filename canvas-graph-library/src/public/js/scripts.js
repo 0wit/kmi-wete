83cfe1174fce52218faba6149c1029d1c0c2b26c
NagasOwit <61089322+0wit@users.noreply.github.com>
2023-02-13 19:04:47 +0100

    Progress in making the graph library.

--- canvas-graph-library/src/public/js/scripts.js
+++ canvas-graph-library/src/public/js/scripts.js
@@ -1,8 +1,32 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(100, 65, 40, 0, 2 * Math.PI);
-ctx.bezierCurveTo(180, 30, 250, 180, 300, 100);
-ctx.stroke();
-ctx.closePath();
-ctx.fill();
+let total = 0;
+let length = 0;
+
+function init()
+{
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    canvas.addEventListener("mousedown", mouseDown(4,5,6,7), false);
+    // canvas.addEventListener("mousemove", mouseXY(), false);
+    // canvas.addEventListener("touchstart", touchDown(), false);
+    // canvas.addEventListener("touchmove", touchXY(), true);
+    // canvas.addEventListener("touchend", touchUp(), false);
+    // document.body.addEventListener("mouseup", mouseUp(), false);
+    // document.body.addEventListener("touchcancel", touchUp(), false);
+}
+
+window.addEventListener("load", init, false);
+
+function mouseDown(...values) {
+    ctx.beginPath();
+    ctx.arc(100, 65, 60, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.fill();
+
+    total = values.reduce((a, b) => a + b, 0);
+    length = values.length;
+    
+    for (value of values) {
+
+    }
+}
