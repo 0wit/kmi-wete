2f138590ce5a83fab2276eb33ec550f5d03a5cc1
NagasOwit <61089322+0wit@users.noreply.github.com>
2023-02-23 12:08:47 +0100

    First version of game of life.

--- game-of-life-s23-b3/src/public/js/scripts.js
+++ game-of-life-s23-b3/src/public/js/scripts.js
@@ -1,6 +1,8 @@
-var w;
+let w;
+let cells = [];
+let height, width = 300 + 50;
 
-function startWorker() {
+function start() {
   if (typeof Worker !== "undefined") {
     if (typeof w == "undefined") {
       w = new Worker("demo_workers.js");
@@ -14,7 +16,59 @@ function startWorker() {
   }
 }
 
-function stopWorker() {
+function stop() {
   w.terminate();
   w = undefined;
 }
+
+function init() {
+
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+
+  for (let i = 0; i < height; i++)
+  {
+    for (let j = 0; j < width; j++)
+    {
+      cells[i][j] = false;
+    }
+  }
+}
+
+function step() {
+
+  for (let i = 1; i < height; i++)
+  {
+    for (let j = 1; j < width; j++)
+    {  
+      aliveNeighbours = 0;
+
+      if (cell[i-1, j]){
+        aliveNeighbours += 1;
+      }
+      if (cell[i+1, j]){
+        aliveNeighbours += 1;
+      }
+      if (cell[i, j-1]){
+        aliveNeighbours += 1;
+      }
+      if (cell[i, j+1]){
+        aliveNeighbours += 1;
+      }
+      
+      if (aliveNeighbours < 2  || aliveNeighbours > 3) {
+        ctx.fillStyle = "white";
+        cells[i][j] = false;
+      }
+
+      if (!cells[i][j] && aliveNeighbours == 3) {
+        ctx.fillStyle = "black";
+        cells[i][j] = true;
+      }
+      
+      ctx.fillRect(i, j, 1, 1);      
+    }
+  }
+}
+
+init();
