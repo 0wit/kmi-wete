e1a9f50b315ceef00f98464fc4c9e1d86633f129
Štěpán Šumpík <61089322+0wit@users.noreply.github.com>
2023-05-17 16:49:24 +0200

    Formatting.

--- game-of-life-s23-b3/src/public/js/scripts.js
+++ game-of-life-s23-b3/src/public/js/scripts.js
@@ -5,23 +5,22 @@ let deadResult;
 let cells = [];
 let aliveCells = [];
 let ctx;
-const gameScale = 4
+const gameScale = 4;
 const height = 800;
 const width = 800;
 
 class Cell {
   constructor(x, y, size, state) {
-      this.x = x;
-      this.y = y;
-      this.size = size;
-      this.state = state;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.state = state;
   }
 }
 
 // create a promise for a worker
 function createWorkerPromise(worker, arrays) {
   return new Promise((resolve, reject) => {
-
     worker.onmessage = (event) => {
       resolve(event.data);
     };
@@ -36,25 +35,23 @@ function createWorkerPromise(worker, arrays) {
 
 // init function that sets the cells based on the desired height and width
 function init() {
-
-  for (let i = 0; i < (height/gameScale); i++)
-  {
+  for (let i = 0; i < height / gameScale; i++) {
     cells[i] = [];
-    for (let j = 0; j < (width/gameScale); j++)
-    {
+    for (let j = 0; j < width / gameScale; j++) {
       cells[i][j] = new Cell(i, j, gameScale, false);
     }
   }
-  canvas = document.getElementById('canvas');
-  canvas.addEventListener("click", function(e){changeCellStateByClick(canvas, e)});
-  ctx = canvas.getContext('2d');
+  canvas = document.getElementById("canvas");
+  canvas.addEventListener("click", function (e) {
+    changeCellStateByClick(canvas, e);
+  });
+  ctx = canvas.getContext("2d");
 }
 
 // buttons' functions
 function start() {
-
-  liveWorker = new Worker('live_worker.js');
-  deadWorker = new Worker('dead_worker.js');
+  liveWorker = new Worker("live_worker.js");
+  deadWorker = new Worker("dead_worker.js");
 
   // const liveWorkerPromise = createWorkerPromise(liveWorker, aliveCells);
   // const deadWorkerPromise = createWorkerPromise(deadWorker, cells, aliveCells);
@@ -68,19 +65,19 @@ function start() {
   // });
 
   liveWorker.postMessage(aliveCells);
-  liveWorker.onmessage = function(event) {
+  liveWorker.onmessage = function (event) {
     liveResult = event.data;
-  }
+  };
 
-const workerData = {
-  cells: cells,
-  aliveCells: aliveCells
-};
+  const workerData = {
+    cells: cells,
+    aliveCells: aliveCells,
+  };
 
   deadWorker.postMessage(workerData);
-  deadWorker.onmessage = function(event) {
+  deadWorker.onmessage = function (event) {
     deadResult = event.data;
-  }
+  };
 }
 
 function stop() {
@@ -98,7 +95,6 @@ function reset() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-
 // cell has died, remove it from array
 function removeAliveCell(cell) {
   for (let i = 0; i < aliveCells.length; i++) {
@@ -111,28 +107,25 @@ function removeAliveCell(cell) {
 }
 
 // manually change state of a specific cell (by click of a mouse)
-function changeCellStateByClick(canvas, e) {  
-
+function changeCellStateByClick(canvas, e) {
   const rect = canvas.getBoundingClientRect();
   let x = Math.floor((e.clientX - rect.left) / gameScale) * gameScale;
   let y = Math.floor((e.clientY - rect.top) / gameScale) * gameScale;
 
-  const i = Math.floor(x / gameScale)
+  const i = Math.floor(x / gameScale);
   const j = Math.floor(y / gameScale);
-  
+
   cells[i][j].state = !cells[i][j].state;
 
   if (cells[i][j].state) {
     ctx.fillStyle = "black";
     aliveCells.push(cells[i][j]);
-  }
-
-  else {
+  } else {
     ctx.fillStyle = "white";
     removeAliveCell(cells[i][j]);
   }
 
-  ctx.fillRect(x, y, 1 * gameScale, 1 * gameScale);  
+  ctx.fillRect(x, y, 1 * gameScale, 1 * gameScale);
 }
 
 init();
